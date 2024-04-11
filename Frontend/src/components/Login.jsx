4f62cf131bf3cd7268dbbc111083c7dcdd922cc2
iianuragii <imanurag18@gmail.com>
2024-04-11 19:40:05 +0530

    Updated Login page

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,40 +1,97 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import { Button, Grid,Box } from '@mui/material';
-import { Link } from 'react-router-dom';
+import * as React from 'react';
+// import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+// import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-    const [log, setLog] = useState("");
-    const getData = async() =>{
-        const response = await axios.get("http://localhost:5000/login")
-        setLog(response.data);
-    }
+// const defaultTheme = createTheme();
+export default function SignIn() {
+  const navigate = useNavigate();
 
-    useEffect(() => {
-      getData();
-    }, [])    
-    
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+    navigate('/chat')  
+  };
+  
   return (
-    <Grid>
-        {log}
-        <Grid sx={{textAlign:'center',pb:'5%',pt:'2%',fontSize:'3rem'}}>
-          Welcome to Anu.ai, an Alpha-AI initiative!!
-        </Grid>
-        <Grid sx={{pl:'33%'}}>
-          <Box sx={{width:'50%',height:'30rem',border: 1,borderColor: 'blue'}}>
-            <Grid sx={{textAlign:'center',pt:'80%'}}>
-              <Button sx={{backgroundColor:'skyblue',borderRadius:'12px','&:hover': {
-                backgroundColor: 'lightblue', // Change this to your desired hover color
-              },width:'7rem'}}>
-                <Link to={'/chat'} >
-                  Login
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >          
+          <Typography component="h1" variant="h5">
+            Welcome to Anu.ai, an Alpha-AI initiative!!
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Link href='/chat'>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              
+            >
+              Sign In
+            </Button>
+            </Link>
+            
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
                 </Link>
-              </Button>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
             </Grid>
           </Box>
-        </Grid>
-    </Grid>
-  )
+        </Box>
+      </Container>
+  );
 }
-
-export default Login
